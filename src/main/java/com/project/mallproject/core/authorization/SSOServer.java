04e2222e5b884abc2a058a4888378adad6521e7d
li xin <2237498407@qq.com>
2021-10-19 20:04:36 +0800

    1.基本完善SSO登陆流程，暂未调试过，后续待完成Logout登出 2.添加责任链 SSOAuthChain

--- src/main/java/com/project/mallproject/core/authorization/SSOServer.java
+++ src/main/java/com/project/mallproject/core/authorization/SSOServer.java
@@ -1,14 +1,21 @@
 package com.project.mallproject.core.authorization;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.project.mallproject.core.enums.ErrorCode;
 import com.project.mallproject.core.exception.CustomException;
+import com.project.mallproject.core.model.login.LoginMessage;
 import com.project.mallproject.core.util.CommonUtil;
+import com.project.mallproject.core.util.EncryptUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import static com.project.mallproject.core.common.Constant.*;
@@ -24,22 +31,76 @@ import static com.project.mallproject.core.common.Constant.*;
 public class SSOServer {
 
     @RequestMapping("/login")
-    public String ssoLogin(String userId, String password, String back, HttpServletResponse response) {
+    public String ssoLogin(String userId, String password, String back, HttpServletResponse response) throws IOException {
         if (!CommonUtil.isNotNull(userId, password)) { // 参数判空
             throw new CustomException(ErrorCode.ERROR_ARG);
         }
         if (!mysql.containsKey(userId) || !password.equals(mysql.get(userId))) { // 数据验证
             throw new CustomException(ErrorCode.ERROR_LOGIN);
         }
+        // todo 添加一个加密的串作为存储的redis value
         String uniqueToken = UUID.randomUUID().toString().trim();
+        redis.put(REDIS_USER_TOKEN + ":" + userId, uniqueToken);
+        // 生成全局 userTicket用户全局门票，代表用户在该CAS端登陆过
+        String userTicket = UUID.randomUUID().toString().trim();
+        // 全局门票
+        setCookie(COOKIE_USER_TICKET + ":" + userId, userId, response);
+        redis.put(REDIS_USER_TICKET + ":" + userTicket, userId);
+        // 生成临时票据，回调给调用端的url
+        String tmpTicket = CommonUtil.createTmpTicket();
+        response.sendRedirect(back + "?tmpTicket=" + tmpTicket);
         return null;
     }
 
+    /*
+     * 验证ticket，拿到token
+     */
     @RequestMapping("/auth")
-    public void ssoAuth(@RequestParam("ticket") String ticket, HttpServletResponse response) {
+    public void ssoAuth(@RequestParam("tmpTicket") String tmpTicket, HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String tmpTicketValue = redis.get(REDIS_TMP_TICKET + ":" + tmpTicket);
+        if (!CommonUtil.isNotNull(tmpTicketValue)) { // 为空，临时票据出错
+            throw new CustomException(ErrorCode.ERROR_TICKET);
+        }
+        String encryptTmpTicket = EncryptUtil.encrypt(tmpTicketValue, MD5);
+        if (!encryptTmpTicket.equals(tmpTicket)) {
+            throw new CustomException(ErrorCode.ERROR_TICKET);
+        } else {
+            redis.remove(REDIS_TMP_TICKET + ":" + tmpTicket); // 验证成功后，将这个tmpTicket从redis中删除
+        }
+        String userTicket = getCookie(request, COOKIE_USER_TICKET);
+        String userId = redis.get(REDIS_USER_TICKET + ":" + userTicket);
+        if (!CommonUtil.isNotNull(userId)) {
+            throw new CustomException(ErrorCode.ERROR_TICKET);
+        }
+        String userRedis = redis.get(REDIS_USER_TOKEN + ":" + userTicket);
+        if (!CommonUtil.isNotNull(userRedis)) {
+            throw new CustomException(ErrorCode.ERROR_TICKET);
+        }
+
+        response.sendRedirect("");
+    }
+
+    @RequestMapping("/logout")
+    public void ssoLogout() {
 
     }
 
     // --------- private function ---------
 
+    private static void setCookie(String key, String val, HttpServletResponse response) {
+        Cookie cookie = new Cookie(key, val);
+        cookie.setDomain("sso.com");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    private static String getCookie(HttpServletRequest request, String key) {
+        Cookie[] cookieList = request.getCookies();
+        for (Cookie cookie : cookieList) {
+            if (key.equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/com/project/mallproject/core/common/Constant.java
+++ src/main/java/com/project/mallproject/core/common/Constant.java
@@ -19,25 +19,32 @@ public class Constant {
         init(); // 准备一些初始数据
     }
 
+    // --------- COMMON SALT VALUE TO ENCRYPT ---------
     public static final String PUBLIC_SHA = "sha-value-is-identitied-in-constant";
 
+    // --------- FIXED SSO URL ---------
     public static final String SSO_LOGIN = "http://localhost:" + PORT + "/sso/login?"; // 没决定要不要
-    public static final String SSO_DO_LOGIN = "http://localhost:" + PORT + "/sso/doLogin?";
+//    public static final String SSO_DO_LOGIN = "http://localhost:" + PORT + "/sso/doLogin?";
     public static final String SSO_AUTH = "http://localhost:" + PORT + "/sso/auth?";
 
+    // --------- FIXED COMMON REDIS KEY ---------
     public static final String COOKIE_USER_TICKET = "cookie_user_ticket";
     public static final String REDIS_USER_TOKEN = "redis_user_token"; // 类似于一个全局session
     public static final String REDIS_USER_TICKET = "redis_user_ticket"; // 全局ticket
     public static final String REDIS_TMP_TICKET = "redis_tmp_ticket"; // 暂时性ticket
 
+
+    // --------- ALGORITHM ---------
     public static final String MD5 = "md5";
     public static final String BASE64 = "base64";
     public static final String SHA = "sha";
 
+    // --------- VIRTUAL STORAGE ---------
     public static Map<String, String> redis = new HashMap<>(16);
     public static Map<String, MySession> ONLINE_USER = new HashMap<>(16);
     public static Map<String, Integer> USER_AUTHORITY = new HashMap<>(16);
     public static Map<String, String> mysql = new HashMap<>(16);
+
     // ------ private function ------
     private static void init() {
         USER_AUTHORITY.put("eddy", 3);
